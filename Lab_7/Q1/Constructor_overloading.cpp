f68284a2df8fc03e6978b8540fddf17395f2d23e
Pranav Agarwal <p2012agarwal@gmail.com>
2020-08-13 12:08:35 +0530

    added lab7 code

---
+++ Lab_7/Q1/Constructor_overloading.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+    using namespace std;
+
+class addition{
+    private:
+        int sum;
+        double sum1;
+        void getsum(double sum, string a){
+            cout<<"\nsum of "<<a<<" is: "<<sum;
+        }
+    public:
+        addition(double var1, double var2){
+            sum1 = var1 + var2;
+            getsum(sum1,"float");
+        }
+        addition(int var1, int var2){
+            sum = var1 + var2;
+            getsum(sum,"int");
+        }
+};
+
+int main(){
+    addition obj(5, 8), obj1(4.11, 7.22);
+}
---
+++ Lab_7/Q2/function_overloading.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int area(int);
+int area(int,int);
+float area(float,float);
+int main()
+{
+	int s,l,b;
+	float bs,ht;
+	cout<<"Enter side of a square:";
+	cin>>s;
+	cout<<"Enter length and breadth of rectangle:";
+	cin>>l>>b;
+	cout<<"Enter base and height of triangle:";
+	cin>>bs>>ht;
+	cout<<"Area of square is"<<area(s);
+	cout<<"\nArea of rectangle is "<<area(l,b);
+    cout<<"\nArea of triangle is "<<area(bs,ht);
+}
+int area(int s)
+{
+    return(s*s);
+}
+int area(int l,int b)
+{
+    return(l*b);
+}
+float area(float bs,float ht)
+{
+   return((bs*ht)/2);
+}
